50fb7cd02f2720bf49b10ef681d9542d566387a8
ardiansah <ardiansah.info@gmail.com>
2022-04-12 16:44:49 +0700

    nonactivate swager

--- Startup.cs
+++ Startup.cs
@@ -27,11 +27,13 @@ namespace ApiWilayahIndonesia
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Wilayah indonesia", Version = "v1" });
-            });
+            services.AddControllersWithViews();
+
+            //comment swager
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Wilayah indonesia", Version = "v1" });
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,21 +42,20 @@ namespace ApiWilayahIndonesia
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Wilayahindonesia v1"));
+
+                //comment swager
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Wilayahindonesia v1"));
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
-            //app.UseSwagger(c =>
-            //{
-            //    c.RouteTemplate = "Service/swagger/{documentName}/swagger.json";
-            //});
-            //
-            //app.UseSwaggerUI(c =>
-            //{
-            //    c.SwaggerEndpoint("/Service/swagger/v1/swagger.json", "TestService");
-            //});
-
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -62,7 +63,9 @@ namespace ApiWilayahIndonesia
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}");
             });
         }
     }
